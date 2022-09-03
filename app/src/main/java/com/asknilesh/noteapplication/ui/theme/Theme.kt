6eaec0c91c4bd1dac6be7a773c1ceb399284b042
AskNilesh <rathodnilsrk@gmail.com>
2022-09-03 12:52:14 +0530

    - First commit - Added required dependencies

---
+++ app/src/main/java/com/asknilesh/noteapplication/ui/theme/Theme.kt
@@ -0,0 +1,23 @@
+package com.asknilesh.noteapplication.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColorScheme(
+  primary = Color.White,
+  background = DarkGray,
+  onBackground = Color.White,
+  surface = LightBlue,
+  onSurface = DarkGray
+)
+
+@Composable
+fun NoteAppTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+  MaterialTheme(
+    colorScheme = DarkColorPalette,
+    typography = Typography,
+    content = content
+  )
+}
